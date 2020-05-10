a930ab2e7a4241a988706894deabd794bb3e892b
charlesat7 <charlesat7@gmail.com>
2020-05-10 16:37:33 +0100

    Update config.py

--- src/config/config.py
+++ src/config/config.py
@@ -5,11 +5,11 @@ config = {
 	'username': 'mytwitchusername',
 
 	# Get your password from http://twitchapps.com/tmi/.
-	'oauth_password': 'oauth:xxxxxxxxxxxxxxxxxxxxxxx',
+	'oauth_password': 'oauth:oauthd20vwpd7ud7gr79gf4ku1ubmvmg5k5',
 
 	# Channel name must start with a "#" followed by an all-lowercase channel name.
 	# Example for joining three separate channels --> 'channels': ['#saltyteemo', '#rtgamecrowd', '#kitboga'],
-	'channels': ['#saltyteemo'],
+	'channels': ['#voidlingrealms'],
 
 	# Display user messages from Twitch Chat.
 	'log_messages': True,
